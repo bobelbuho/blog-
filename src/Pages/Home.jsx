a5ea64ae2f8c32369fa95c823b8b1fdd5dc82888
Bob <bob.vaz@hotmail.fr>
2024-03-18 21:11:12 +0100

    init components

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Header from '../Components/Header'
+import Search from '../Components/Search'
+import IntroPost from '../Components/IntroPost'
+import Blogs from '../Components/Blogs'
+import Footer from '../Components/Footer'
+
+function Home() {
+    return (
+        <div>
+            {/* Header */}
+            <Header />
+            {/* Search */}
+            <Search />
+            {/* IntroPost */}
+            <IntroPost />
+            {/* Blogs */}
+            <Blogs />
+            {/* Footer */}
+            <Footer />
+        </div>
+    )
+}
+
+export default Home
