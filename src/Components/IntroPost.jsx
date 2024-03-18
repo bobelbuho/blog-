204a814bbedb8597433a17c7db2f75a78ed20fea
Bob <bob.vaz@hotmail.fr>
2024-03-18 22:08:05 +0100

    init api

--- src/Components/IntroPost.jsx
+++ src/Components/IntroPost.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import GlobalApi from '../Services/GlobalApi';
 
 function IntroPost() {
+  useEffect(()=>{
+    getPost()
+}, [])
+  const getPost=()=>{
+  GlobalApi.getPost.then(resp=>{
+    console.log(resp.data.data);
+  })}
   return (
     <div>IntroPost</div>
   )
