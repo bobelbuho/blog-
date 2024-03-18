22ef3f24a09656ca04c1426a329999bdadae3a5f
Bob <bob.vaz@hotmail.fr>
2024-03-18 21:59:14 +0100

    search okk

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import banner from './../assets/Images/banner.jpg'
 import { IoSearchOutline } from "react-icons/io5";
-//import GlobalApi from '../Services/GlobalApi';
+import GlobalApi from '../Services/GlobalApi';
 function Search({selectedTag}) {
     const tags=[
         {
