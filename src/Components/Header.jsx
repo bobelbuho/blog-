14ea1417fa525d7e85d935fb2bdaa3a5e7aa6331
Bob <bob.vaz@hotmail.fr>
2024-03-18 21:25:24 +0100

    header ok

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import logo from './../assets/Images/logo.png'
+import { AiFillYoutube } from "react-icons/ai";
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='flex justify-between items-center'>
+      <img src={logo} className='w-[180px]'/>
+      <ul className='flex gap-4 md:gap-14 '>
+        <li className='hover:font-bold cursor-pointer'>Home</li>
+        <li className='hover:font-bold cursor-pointer'>About Us</li>
+        <li className='hover:font-bold cursor-pointer'>Contact Us</li>
+      </ul>
+      <button className='bg-red-500 rounded-full text-white flex items-center'>
+        Subscribe <AiFillYoutube className='ml-3 text-[20px]'/>
+        </button>
+    </div>
   )
 }
 
